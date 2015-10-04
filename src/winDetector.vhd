025a3060cf02745c6759151647be3707f3e4a2b8
Rocket50 <rocket-50@hotmail.com>
2015-10-03 23:18:12 -0700

    Fixed winDetector, added working winDetector testbench

--- src/winDetector.vhd
+++ src/winDetector.vhd
@@ -1,12 +1,13 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 package winDetectorDec is
-  component winDetector is
-    port(spin_result : in std_logic_vector(5 downto 0);
-         bet1_value : in std_logic_vector(5 downto 0);
+  component win is
+    port(spin_result : in unsigned(5 downto 0);
+         bet1_value : in unsigned(5 downto 0);
          bet2_colour: in std_logic;
-         bet3_dozen: in std_logic_vector(1 downto 0);
+         bet3_dozen: in unsigned(1 downto 0);
          bet1_wins : out std_logic;
          bet2_wins : out std_logic;
          bet3_wins : out std_logic);
@@ -17,17 +18,17 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity winDetector is
-  port(spin_result : in std_logic_vector(5 downto 0);
-         bet1_value : in std_logic_vector(5 downto 0);
+entity win is
+  port(spin_result : in unsigned(5 downto 0);
+         bet1_value : in unsigned(5 downto 0);
          bet2_colour: in std_logic;
-         bet3_dozen: in std_logic_vector(1 downto 0);
+         bet3_dozen: in unsigned(1 downto 0);
          bet1_wins : out std_logic;
          bet2_wins : out std_logic;
          bet3_wins : out std_logic);
 end entity; 
 
-architecture impl of winDetector is
+architecture impl of win is
   begin
     --Straightup bet subblock
     process(all) begin
@@ -46,11 +47,14 @@ architecture impl of winDetector is
     
     begin
 
-      unsg_result := unsigned(spin_result); 
+      unsg_result := spin_result; 
+      --black = 0
+      --red = 1
       
-      if(spin_result'RIGHT = 0) then
+      if(spin_result(0) = '0') then
         if((unsg_result >= 1 and unsg_result <= 10) or (unsg_result >= 19 and unsg_result <= 28)) then
           winning_col := '0';
+          
         else
           winning_col := '1';
         end if;
@@ -63,24 +67,29 @@ architecture impl of winDetector is
       end if;
       
       if(winning_col = bet2_colour) then
-        bet3_wins <= '1'; 
+        bet2_wins <= '1'; 
       else
-        bet3_wins <= '0'; 
+        bet2_wins <= '0'; 
+      end if;
+      
+      if(unsg_result = 0) then
+        bet2_wins <= '0';
       end if;
 
     end process;
     
     --dozen bet subblock
     process(all) begin 
-      if(bet3_dozen = "00" and spin_result = 6d"1" and spin_result = 6d"12") then
+      report("Bet 3 dozen : " & to_String(bet3_dozen));
+      if(bet3_dozen = "00" and spin_result >= 6d"1" and spin_result <= 6d"12") then
         bet3_wins <= '1'; 
-      elsif(bet3_dozen = "01" and (spin_result = 6d"13" and spin_result = 6d"24")) then
+      elsif(bet3_dozen = "01" and (spin_result >= 6d"13" and spin_result <= 6d"24")) then
         bet3_wins <= '1'; 
-      elsif(bet3_dozen = "10" and (spin_result = 6d"25" and spin_result = 6d"36")) then
+      elsif(bet3_dozen = "10" and (spin_result >= 6d"25" and spin_result <= 6d"36")) then
         bet3_wins <= '1'; 
       else
         bet3_wins <= '0';
+        
       end if;
-      
     end process; 
 end impl;
